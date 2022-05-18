72966f70a3635374f0afe60b8be748a19a8a1544
Jason Baik <baikjs87@gmail.com>
2022-05-18 01:03:10 -0400

    WIP

--- index.js
+++ index.js
@@ -11,13 +11,76 @@
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+        return e.date;
+    });
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    const payable = eligibleDates.reduce(
+        function (memo, d) {
+            return memo + wagesEarnedOnDate.call(this, d);
+        }.bind(this),
+        0
+    ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+    return payable;
+};
+
+function createEmployeeRecord(employee) {
+    const employeeRecord = [];
+    employeeRecord.firstName = employee[0];
+    employeeRecord.familyName = employee[1];
+    employeeRecord.title = employee[2];
+    employeeRecord.payPerHour = employee[3];
+    employeeRecord.timeInEvents = [];
+    employeeRecord.timeOutEvents = [];
+    // console.log(employeeRecord);
+    return employeeRecord;
 }
 
+function createEmployeeRecords(records) {
+    const employeeRecords = [];
+    for (const data of records) {
+        employeeRecords.push(createEmployeeRecord(data));
+    }
+    return employeeRecords;
+}
+
+function createTimeInEvent(dateTime) {
+    this.timeInEvents.push({ type: "TimeIn" });
+    const timeIn = this.timeInEvents[0];
+    const timeInDate = dateTime.split(" ")[0];
+    const timeInHour = parseInt(dateTime.split(" ")[1]);
+    timeIn.date = timeInDate;
+    timeIn.hour = timeInHour;
+    return this;
+}
+
+function createTimeOutEvent(dateTime) {
+    this.timeOutEvents.push({ type: "TimeOut" });
+    const timeOut = this.timeOutEvents[0];
+    const timeOutDate = dateTime.split(" ")[0];
+    const timeOutHour = parseInt(dateTime.split(" ")[1]);
+    timeOut.date = timeOutDate;
+    timeOut.hour = timeOutHour;
+    return this;
+}
+
+function hoursWorkedOnDate(workDate) {
+    for (let i = 0; i < this.timeInEvents.length; i++) {
+        if (this.timeInEvents[i].date === workDate) {
+            const hours =
+                (this.timeOutEvents[i].hour - this.timeInEvents[i].hour) / 100;
+            return hours;
+        }
+    }
+}
+
+function wagesEarnedOnDate(workDate) {
+    const hoursWorked = hoursWorkedOnDate.call(workDate);
+    for (let i = 0; i < this.timeInEvents.length; i++) {
+        if (this.timeInEvents[i].date === workDate) {
+            console.log(this.payPerHour * hoursWorked);
+        }
+    }
+    // console.log(this);
+    // console.log(workDate);
+}
--- test/helpers.js
+++ test/helpers.js
@@ -1,24 +1,29 @@
-const chai = require('chai')
-global.expect = chai.expect
-global.chai = chai
-const fs = require('file-system')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-const babel = require('babel-core');
-const spies = require('chai-spies')
+const chai = require("chai");
+global.expect = chai.expect;
+global.chai = chai;
+const fs = require("file-system");
+const jsdom = require("mocha-jsdom");
+const path = require("path");
+const babel = require("babel-core");
+const spies = require("chai-spies");
 
-chai.use( spies );
+chai.use(spies);
 
-const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
-
-const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
-  }
+const html = fs.readFileSync(
+    path.resolve(__dirname, "..", "index.html"),
+    "utf-8"
 );
 
-const src = babelResult.code
+const babelResult = babel.transformFileSync(
+    path.resolve(__dirname, "..", "index.js"),
+    {
+        presets: ["env"],
+    }
+);
+
+const src = babelResult.code;
 
 jsdom({
-  html, src
+    html,
+    src,
 });
